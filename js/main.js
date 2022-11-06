43c8c2b0e4ea048be1b11258089a79a55f183474
ultracire <ericphan43@gmail.com>
2022-11-06 10:48:51 -0800

    new files

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-document.querySelector("#clickMe").addEventListener("click", makeReq);
-
+// in order to call function you need to define it first
 async function makeReq() {
-  const userName = document.querySelector("#userName").value;
-  const pokemon = document.querySelector("#pokemon").value;
-  const res = await fetch(`/api?student=${userName}&pokemon=${pokemon}`);
-  const data = await res.json();
+  _input = document.querySelector("input").value;
 
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name;
-  document.querySelector("#personStatus").textContent = data.status;
-  document.querySelector("#personOccupation").textContent =
-    data.currentOccupation;
+  console.log(_input);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${_input}`);
+  const data = await res.json();
+  // console.log(data);
+  document.getElementsByClassName("normal").src = data.sprites.front_default;
+
+  return data;
 }
+
+// async function modifyDOM(data) {
+//   console.log(data.sprites.front_default);
+//   document.getElementsByClassName("normal").src = data.sprites.front_default;
+// }
+
+document.querySelector("button").addEventListener("click", makeReq);
+const data = makeReq();
+console.log(data);
+// modifyDOM(data);
+// document.getElementsByClassName("normal").src = data.sprites.front_default;
